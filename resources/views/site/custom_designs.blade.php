f00e7dcee51f17dd5658d36e76e54bd0f258b6fb
Mahmoud Nagy <mahmoud-nagy>
2024-10-14 00:17:50 +0300

    fixes

--- resources/views/site/custom_designs.blade.php
+++ resources/views/site/custom_designs.blade.php
@@ -61,6 +61,7 @@
                                             <img class="img-fluid pad" alt="design" id="design_front"
                                                  src="{{Storage::url('designs/'.$design->design_image_front)}}"
                                                  style=" width: {{$record->site_front_width}}%; height: {{$record->site_front_height}}%; top: {{$record->site_front_top}}%; left: {{$record->site_front_left}}%;position: absolute;">
+                                        @endif
                                         @if($design->design_image_back)
                                             <img class="img-fluid pad d-none" alt="design" id="design_back"
                                                  src="{{Storage::url('designs/'.$design->design_image_back)}}"
@@ -535,8 +536,9 @@
         });
 
         function submitValues(key) {
-            $('#'+key).val(this.value);
+            $('#' + key).val(this.value);
         }
+
         function removeCart(e) {
             e.closest('#size_form').remove();
             changeQty();
